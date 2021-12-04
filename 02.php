e7b3c7cc99116ccab63c1f7d991bcd023700da70
mariush-github <52931811+mariush-github@users.noreply.github.com>
2021-12-04 11:49:28 +0200

    Update 02.php

--- 02.php
+++ 02.php
@@ -33,20 +33,3 @@ foreach ($commands as $command) {
 echo "02.02 x=$x y=$y result=".($x*$y)."\n";
 
 ?>
-
-
-$counter = 0;
-for ($i=1;$i<count($numbers);$i++) {
-	$counter += ($numbers[$i]>$numbers[$i-1]) ? 1 : 0;
-}
-echo "01.01 solution : $counter \n";
-
-$previous = $numbers[0]+$numbers[1]+$numbers[2];
-$counter = 0;
-for ($i=2;$i<count($numbers)-1;$i++) {
-	$sum = $previous - $numbers[$i-2] + $numbers[$i+1];
-	$counter += ($sum>$previous) ? 1 : 0;
-	$previous = $sum;
-}
-echo "01.02 solution : $counter \n";
-?>
